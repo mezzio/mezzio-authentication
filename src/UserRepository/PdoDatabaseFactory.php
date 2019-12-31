6662a5bdb52c37b3383c8a1505f29cb484114827
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:42:29 -0600

    Rewriting as Laminas Project package

--- src/UserRepository/PdoDatabaseFactory.php
+++ src/UserRepository/PdoDatabaseFactory.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-authentication for the canonical source repository
- * @copyright Copyright (c) 2017-2018 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-authentication/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-authentication for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-authentication/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-authentication/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace Zend\Expressive\Authentication\UserRepository;
+namespace Mezzio\Authentication\UserRepository;
 
+use Mezzio\Authentication\Exception;
+use Mezzio\Authentication\UserInterface;
 use PDO;
 use Psr\Container\ContainerInterface;
-use Zend\Expressive\Authentication\Exception;
-use Zend\Expressive\Authentication\UserInterface;
 
 class PdoDatabaseFactory
 {
